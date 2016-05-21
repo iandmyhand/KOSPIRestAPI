5a37d058b266610f87f4d04e1f6cb35b1094ece1
SeomGi, Han <iandmyhand@gmail.com>
2016-05-22 03:27:01 +0900

    Add gradle script to build as war. #1

--- src/main/java/com/blackvelvet/KospiApiController.java
+++ src/main/java/com/blackvelvet/KospiApiController.java
@@ -1,24 +1,14 @@
 package com.blackvelvet;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class KospiApiController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!!";
+    @RequestMapping("/ping")
+    public String ping() {
+        return "OK";
     }
-
-    public static void main(String[] args) {
-		SpringApplication.run(KospiApiController.class, args);
-	}
 
 }
--- src/test/java/com/blackvelvet/KospiApiControllerTests.java
+++ src/test/java/com/blackvelvet/KospiApiControllerTests.java
@@ -1,15 +1,19 @@
 package com.blackvelvet;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.internal.exceptions.ExceptionIncludingMockitoWarnings;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -18,7 +22,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @WebAppConfiguration
 public class KospiApiControllerTests {
 
-    private MockMvc mvc;
+	private MockMvc mvc;
 
     @Before
     public void setUp() throws Exception {
@@ -26,10 +30,16 @@ public class KospiApiControllerTests {
     }
 
 	@Test
-	public void getHello() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(equalTo("Hello World!!")));
+	public void getPing() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/ping").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("OK")));
 	}
+
+    @Test
+    public void get404() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/404").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 
 }
