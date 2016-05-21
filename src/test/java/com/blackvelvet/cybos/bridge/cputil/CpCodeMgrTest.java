fa1c44171f676aec4fff00c5bac36223e170b9a3
SeomGi, Han <iandmyhand@gmail.com>
2016-05-22 03:52:45 +0900

    Add bridge classes for Cybos Plus DLLs. #1

---
+++ src/test/java/com/blackvelvet/cybos/bridge/cputil/CpCodeMgrTest.java
@@ -0,0 +1,40 @@
+package com.blackvelvet.cybos.bridge.cputil;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CpCodeMgrTest {
+
+	private ICpCodeMgr iCpCodeMgr;
+	private CpCodeMgr cpCodeMgr;
+
+	@Before
+	public void setUp() throws Exception {
+		if(null == iCpCodeMgr) iCpCodeMgr = ClassFactory.createCpCodeMgr();
+		if(null == cpCodeMgr) cpCodeMgr = new CpCodeMgr();
+	}
+
+	@Test
+	public void testIGetStockListByMarket() {
+		Object[] objs = (Object[]) iCpCodeMgr.getStockListByMarket(CPE_MARKET_KIND.CPC_MARKET_KOSPI);
+		assertEquals("String", objs[0].getClass().getSimpleName());
+		
+		assertTrue(0 < objs.length);
+		System.out.println(objs.length);
+		
+		assertEquals("A000020", (String)objs[0]);
+		System.out.println((String)objs[0]);
+	}
+
+	@Test
+	public void testGetStockListByMarket() {
+		List<String> codes = cpCodeMgr.getStockListByMarket(CPE_MARKET_KIND.CPC_MARKET_KOSPI);
+		assertTrue(0 < codes.size());		
+		assertEquals("A000020", codes.get(0));
+	}
+
+}
---
+++ src/test/java/com/blackvelvet/cybos/bridge/cputil/CpCybosTest.java
@@ -0,0 +1,22 @@
+package com.blackvelvet.cybos.bridge.cputil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CpCybosTest {
+
+	private ICpCybos cpCybos;
+
+	@Before
+	public void setUp() throws Exception {
+		cpCybos = ClassFactory.createCpCybos();
+	}
+
+	@Test
+	public void testIsConnect() {
+		assertEquals(1, cpCybos.isConnect());
+	}
+
+}
